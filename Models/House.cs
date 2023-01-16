757d937685d8e5de93e0de2d549be95e3ac42f1d
Alejandro Clemente <alejandro.clemente.i@gmail.com>
2023-01-16 17:27:44 -0300

    Adding initial project with fluent api using EF

---
+++ Models/House.cs
@@ -0,0 +1,23 @@
+using System.Text.Json.Serialization;
+
+namespace houses.Models;
+
+public class House
+{
+    public Guid HouseId { get; set; }
+    public string Title { get; set; }
+    public string Motto { get; set; }
+    public string? Description { get; set; }
+    public string? ShieldUrl { get; set; }
+    public Influence HouseInfluence { get; set; }
+    public DateTime StartDate { get; set; }
+    [JsonIgnore]
+    public virtual ICollection<Person> People {get; set;}
+}
+
+public enum Influence
+{
+    Low,
+    Middle,
+    Hight
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+using houses;
+using houses.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+// builder.Services.AddDbContext<EFContext>(x => x.UseInMemoryDatabase("HousesDB"));
+builder.Services.AddSqlServer<HousesContext>(builder.Configuration.GetConnectionString("cnnHouses"));
+
+var app = builder.Build();
+
+app.MapGet("/dbConnection", async ([FromServices] HousesContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok($"Database created: {dbContext.Database.IsSqlServer()}");
+});
+
+app.MapGet("/api/houses", async ([FromServices] HousesContext dbContext) =>
+{
+    return Results.Ok(dbContext.Houses);
+});
+
+app.MapPost("/api/houses", async ([FromServices] HousesContext dbContext, [FromBody] House house) =>
+{
+    house.HouseId = Guid.NewGuid();
+    await dbContext.Houses.AddAsync(house);
+    await dbContext.SaveChangesAsync();
+    return Results.Ok("House created");
+});
+
+app.MapPut("/api/houses/{id}", async ([FromServices] HousesContext dbContext, [FromBody] House house, [FromRoute] Guid id) =>
+{
+    var currentHouse = await dbContext.Houses.FindAsync(id);
+    if (currentHouse != null)
+    {
+        currentHouse.Title = house.Title;
+        currentHouse.Motto = house.Motto;
+        currentHouse.HouseInfluence = house.HouseInfluence;
+
+        await dbContext.SaveChangesAsync();
+        return Results.Ok("House updated");
+    }
+
+    return Results.NotFound("The house is not found");
+
+});
+
+app.MapDelete("/api/houses/{id}", async ([FromServices] HousesContext dbContext, [FromRoute] Guid id) =>
+{
+    var currentHouse = await dbContext.Houses.FindAsync(id);
+    if (currentHouse != null)
+    {
+        dbContext.Houses.Remove(currentHouse);
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok("House deleted");
+    }
+
+    return Results.NotFound("The house is not found");
+
+});
+
+app.MapGet("/api/people", async ([FromServices] HousesContext dbContext) =>
+{
+    return Results.Ok(dbContext.People.Include(p => p.House));
+});
+
+app.MapPost("/api/people", async ([FromServices] HousesContext dbContext, [FromBody] Person person) =>
+{
+    person.PersonId = Guid.NewGuid();
+    await dbContext.People.AddAsync(person);
+    await dbContext.SaveChangesAsync();
+    return Results.Ok("Person created");
+});
+
+app.Run();
